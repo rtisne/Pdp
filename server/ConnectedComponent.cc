06a1c4fc4a0928c64e99c7130993bb8e6dc9757b
Shqrk <thomaslabrousse@orange.fr>
2016-02-16 08:50:58 +0100

    InitBB()

--- server/ConnectedComponent.cc
+++ server/ConnectedComponent.cc
@@ -18,12 +18,39 @@ std::vector<cv::Point> ConnectedComponent::getListP()
 }
 
 
-void ConnectedComponent::initBB()
-{
-	
-}
 
 int ConnectedComponent::sizeListP()
 {
 	return ListP.size();
 }
+
+void ConnectedComponent::initBB()
+{
+	cv::Point P_Max;
+	 P_Max.x = ListP[0].x;
+	 P_Max.y = ListP[0].y;
+	cv::Point P_Min;
+	 P_Min.x = ListP[0].x;
+	 P_Min.y = ListP[0].y;
+          for(int i= 1; i < ListP.size()-1; i++)
+            {
+                  cv::Point P = ListP[i];
+                  if(P.y < P_Min.y && P.x < P_Min.x)
+                  {
+                  	P_Min.y = P.y;
+                  	P_Min.x = P.x;
+                  }
+
+                 if(P.y > P_Max.y && P.x > P_Max.x)
+                  {
+                  	P_Min.y = P.y;
+                  	P_Min.x = P.x;
+                  }
+
+
+            }
+
+    BB.setX(P_Max);
+    BB.setY(P_Min);
+}
+
